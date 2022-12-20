1447eadc5ba22b470671af4f307371b06f5b739e
True_ensyuritu <sun3791888@gmail.com>
2022-12-20 20:15:49 +0900

    えぺらんだまいざ追加

---
+++ randomizer.js
@@ -0,0 +1,57 @@
+const legends = [
+    "ブラッドハウンド",
+    "ジブラルタル",
+    "ライフライン",
+    "パスファインダー",
+    "レイス",
+    "バンガロール",
+    "ミラージュ",
+    "コースティック",
+    "オクタン",
+    "ワットソン",
+    "クリプト",
+    "レブナント",
+    "ローバ",
+    "ランパート",
+    "ホライゾン",
+    "ヒューズ",
+    "ヴァルキリー",
+    "シア",
+    "アッシュ",
+    "マッドマギー",
+    "ニューキャッスル"]
+
+const weapons = [
+    "P2020",
+    "RE-45オート",
+    "オルタネーターSMG",
+    "G7スカウト",
+    "R99 SMG",
+    "R301カービン",
+    "スピットファイア",
+    "CAR SMG",
+    "ランページLMG",
+    "30-30リピーター",
+    "ウィングマン",
+    "プラウラー",
+    "ヘムロック",
+    "フラットライン",
+    "ボルトSMG",
+    "ハボック",
+    "ディヴォーション",
+    "モザンビーク",
+    "マスティフ",
+    "ピースキーパー",
+    "EVA8 オート",
+    "センチネル",
+    "チャージライフル",
+    "トリプルテイク",
+    "ロングボウDMR",
+    "クレーバー"
+]
+
+function randomize() {
+    legend.textContent = (legends[Math.floor(Math.random() * legends.length)])
+    weapon1.textContent = (weapons[Math.floor(Math.random() * weapons.length)])
+    weapon2.textContent = (weapons[Math.floor(Math.random() * weapons.length)])
+}
