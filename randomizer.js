b19491100541d1acbf1d584e64b302aa3999202d
True_ensyuritu <sun3791888@gmail.com>
2022-12-24 21:53:00 +0900

    上級者向け設定項目を追加、SPINボタンを追加

--- randomizer.js
+++ randomizer.js
@@ -53,17 +53,16 @@ const weapons = [
 ]
 
 let rnd = ((Date.now()*141350357)%4294967295)-2147483648;
-let a = 7
-let b = 13
-let c = 17
+let a = 13
+let b = 17
+let c = 5
 let weaponDuplicationAvaliable = true
 let params = (new URL(document.location)).searchParams;
-if(params.has("seed") && parseInt(params.get("seed")) != NaN){
-    rnd = parseInt(params.get("seed"))
-    seedMessage.textContent = "初期シード値["+rnd+"]が適用されました。"
-}else{
-    seedMessage.textContent = "現在時刻に基づき、初期シード値["+rnd+"]が適用されました。"
-}
+let intervalDelay = 33
+let spinning = false
+let spinInterval = 0
+
+seedMessage.textContent = "現在時刻に基づき、初期シード値["+rnd+"]が適用されています。"
 if(params.has("a") && parseInt(params.get("a")) != NaN) a = parseInt(params.get("a"))
 if(params.has("b") && parseInt(params.get("b")) != NaN) b = parseInt(params.get("b"))
 if(params.has("c") && parseInt(params.get("c")) != NaN) c = parseInt(params.get("c"))
@@ -72,6 +71,11 @@ console.log(a)
 console.log(b)
 console.log(c)
 seed.value = rnd
+shift1.value = a
+shift2.value = b
+shift3.value = c
+rouletteDelay.value = intervalDelay
+shiftMessage.textContent = "現在のビットシフトの組み合わせは["+a+"] ["+b+"] ["+c+"] です。"
 
 
 function getRnd(){
@@ -107,6 +111,54 @@ function changeWeaponDuplication(){
     }
 }
 
+function sethift(){
+    const ta = shift1.value
+    const tb = shift2.value
+    const tc = shift3.value
+    if(isNaN(parseInt(shift1.value) + parseInt(shift2.value) + parseInt(shift3.value))){
+        shiftMessage.textContent = "数値を入力してください。"
+        return
+    }else if(ta == tb || tb == tc || tc == ta){
+        shiftMessage.textContent = "同じ値がある場合適用できません。"
+        return
+    }
+    a = ta
+    b = tb
+    c = tc
+    shiftMessage.textContent = "ビットシフト["+a+"] ["+b+"] ["+c+"]が適用されました。"
+}
+
+function setAdvanced(){
+    let elements = document.getElementsByClassName("advanced")
+    activateAdvanced.hidden = "true"
+    for(element of elements){
+        element.hidden = false
+    }
+}
+
+function setRouletteDelay(){
+    const s = parseInt(rouletteDelay.value)
+    if(isNaN(s)){
+        rouletteDelayMessage.textContent = "数値を入力してください。"
+    }else{
+        rouletteDelayMessage.textContent = "抽選間隔["+s+"]msが適用されました。"
+        intervalDelay = s
+    }
+}
+
+function roulette(){
+    console.log(spinning)
+    if(!spinning){
+        spinInterval = setInterval(randomize, intervalDelay)
+        rouletteButton.textContent = "STOP"
+        spinning = true
+    }else{
+        clearInterval(spinInterval)
+        rouletteButton.textContent = "SPIN"
+        spinning = false
+    }
+}
+
 function randomize() {
     currentSeed.textContent = "この構成のシード値: "+rnd
     legend.textContent = (legends[Math.floor(getRnd() * legends.length)])
@@ -122,6 +174,6 @@ function randomize() {
             wp2 = (weapons[Math.floor(getRnd() * weapons.length)])
         }
     }
-    weapon1.textContent = wp1
-    weapon2.textContent = wp2
+    weapon1.textContent = "\u3000"+wp1+"\u3000"
+    weapon2.textContent = "\u3000"+wp2+"\u3000"
 }
